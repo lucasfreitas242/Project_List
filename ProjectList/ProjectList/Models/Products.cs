1e586957ff04f2c77b95917b9f9ef61ebc53753e
lucasfreitas242 <lucasfrittas@gmail.com>
2021-08-24 20:25:51 -0300

    Criação da Model Products e adição do Context de Banco de Dados

---
+++ ProjectList/ProjectList/Models/Products.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjectList.Models
+{
+    public class Products
+    {
+        public long ProductCode { get; set; }
+
+        public int Quantity { get; set; }
+    }
+}
